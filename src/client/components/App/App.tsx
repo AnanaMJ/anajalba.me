de8598aa6ca44bc537c4ed91b63ce99b96aecff2
Ana Jalba <ana.jalba13@gmail.com>
2018-12-16 18:52:27 +0000

    Add navbar

--- src/client/components/App/App.tsx
+++ src/client/components/App/App.tsx
@@ -3,6 +3,9 @@ import {ReactNode} from "react";
 import {BrowserRouter, Link, Route, Switch} from "react-router-dom";
 import {HomePage} from "../HomePage/HomePage";
 import {AboutPage} from "../AboutPage/AboutPage";
+import * as bs from "../../global-styles/Bootstrap.scss";
+import * as styles from "./App.scss";
+import {combine} from "../../helpers/style-helpers";
 
 class App extends React.PureComponent {
 
@@ -10,10 +13,12 @@ class App extends React.PureComponent {
 		return (
 			<BrowserRouter>
 				<>
-					<ul>
-						<li><Link to={"/"}>home</Link></li>
-						<li><Link to={"/about"}>about</Link></li>
-					</ul>
+					<nav className={combine(styles.siteHeader, bs.stickyTop, bs.py2)}>
+						<div className={combine(bs.containerFluid, bs.dFlex, bs.flexColumn, bs.flexMdRow, bs.justifyContentStart)}>
+							<Link className={bs.px3} to={"/"}>Home</Link>
+							<Link className={bs.px3} to={"/about"}>About</Link>
+						</div>
+					</nav>
 					<Switch>
 						<Route path={"/"} component={HomePage} exact={true}/>
 						<Route path={"/about"} component={AboutPage}/>
---
+++ src/client/helpers/style-helpers.ts
@@ -0,0 +1,8 @@
+function combine(...classNames: Array<string | boolean | undefined>): string {
+	// concat all string items
+	return classNames.filter((element) => typeof element === typeof "").join(" ");
+}
+
+export {
+	combine,
+};
