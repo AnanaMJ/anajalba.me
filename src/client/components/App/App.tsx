058f8222e86df46a4898d4ad9357d8f258a3641a
Ana Maria Jalba <ana.jalba13@gmail.com>
2020-11-28 18:11:33 +0000

    Make more containers and put the front page up

--- src/client/components/App/App.tsx
+++ src/client/components/App/App.tsx
@@ -4,20 +4,23 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { HomePage } from "../HomePage";
 import { AboutPage } from "../AboutPage/AboutPage";
 import { NavBar, MobileNav } from "../Nav";
+import * as styles from "./App.scss";
 
 export class App extends PureComponent {
   public render(): ReactNode {
     return (
       <BrowserRouter>
-        <>
+        <div className={styles.page}>
           <MobileNav />
           <NavBar />
 
-          <Switch>
-            <Route path={"/"} component={HomePage} exact={true} />
-            <Route path={"/about"} component={AboutPage} />
-          </Switch>
-        </>
+          <div className={styles.container}>
+            <Switch>
+              <Route path={"/"} component={HomePage} exact={true} />
+              <Route path={"/about"} component={AboutPage} />
+            </Switch>
+          </div>
+        </div>
       </BrowserRouter>
     );
   }
--- src/client/components/HomePage/HomePage.tsx
+++ src/client/components/HomePage/HomePage.tsx
@@ -1,21 +1,23 @@
 import React, { ReactNode, PureComponent } from "react";
-import { faHome } from "@fortawesome/pro-light-svg-icons";
+import { faConstruction } from "@fortawesome/pro-light-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import * as bs from "../../global-styles/Bootstrap.scss";
-import * as style from "./HomePage.scss";
+// import * as style from "./HomePage.scss";
 
 export class HomePage extends PureComponent {
   public render(): ReactNode {
     return (
-      <div className={style.container}>
+      <>
         <h1>
+          This is <b>still</b> a site in progress. <FontAwesomeIcon className={bs.ml2} icon={faConstruction} />
+        </h1>
+        {/* <h1>
           Home Page
           <FontAwesomeIcon className={bs.ml2} icon={faHome} />
         </h1>
-
-        <p>This is Home Page.</p>
-      </div>
+        <p>Welcome to my page.</p> */}
+      </>
     );
   }
 }
